204435599b4cfe0e245edda5c87c19093e283b2b
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-11 18:56:59 +0600

    Removed the very first comment as it gave Rest API error

--- university-post-types.php
+++ university-post-types.php
@@ -1,9 +1,8 @@
-<!-- We put the custom post type code into mu-plugin folder so the owner can't disable the custom post types from the wp dashboard -->
-
 <?php
 function university_post_types()
 {
     register_post_type("event", array(
+        'show_in_rest' => true, //This will use the modern block editor
         "public" => true, //Now we can manage events from wp dashboard
         "supports" => array("title", "editor", "excerpt"), //lets us add fields that we want
         "rewrite" => array("slug" => "events"), //changes the slug to events
@@ -18,10 +17,11 @@ function university_post_types()
             "search_items" => "Search Events",
             "view_item" => "View Event",
         ),
-        'show_in_rest' => true, //This will use the modern block editor
         "menu_icon" => "dashicons-calendar", //This will change the icon of the Events menu, we can get icons from wp dashicons
         "has_archive" => true //Where it should have an archive page
     ));
 }
 
 add_action("init", "university_post_types");
+
+?>
