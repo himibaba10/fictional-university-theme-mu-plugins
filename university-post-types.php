c2dfb8cdd32fb8d472814e14ed8ec8341e048fef
Ferdous Ahmed <himibaba10@gmail.com>
2025-08-08 18:34:35 +0600

    Created program post type

--- university-post-types.php
+++ university-post-types.php
@@ -5,22 +5,43 @@ function university_post_types()
     register_post_type("event", array(
         "public" => true,
         "has_archive" => true,
-        "supports" => array(
+        "supports" => [
             "title",
             "editor",
             "excerpt",
-        ),
+        ],
         "rewrite" => array(
             "slug" => "events"
         ),
-        "labels" => array(
+        "labels" => [
             "name" => "Events",
             "add_new_item" => "Add Event",
             "edit_item" => "Edit Event",
             "all_items" => "All Events",
-            "singular_name" => "Event",
-        ),
+            "singular_name" => "Event"
+        ],
         'menu_icon' => "dashicons-calendar",
+        'show_in_rest' => true, //for modern editor
+    ));
+
+    register_post_type("program", array(
+        "public" => true,
+        "has_archive" => true,
+        "supports" => [
+            "title",
+            "editor"
+        ],
+        "rewrite" => array(
+            "slug" => "programs"
+        ),
+        "labels" => [
+            "name" => "Programs",
+            "add_new_item" => "Add Program",
+            "edit_item" => "Edit Program",
+            "all_items" => "All Programs",
+            "singular_name" => "Program"
+        ],
+        'menu_icon' => "dashicons-awards",
         'show_in_rest' => true, //for modern editor
     ));
 }
